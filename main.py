b508b58340556d912119a852052805b69c1e6b34
Knowledge Mhodi <99808976+mhodieknowledge@users.noreply.github.com>
2025-12-15 13:53:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -611,4 +611,5 @@ def scrape_all():
     return jsonify({"message": "Scraping all feeds completed", "results": results}), 200
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
